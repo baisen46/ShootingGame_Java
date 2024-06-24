3ee398fee77db267e827b3c812a25b18280bc386
chiba <baisen46@gmail.com>
2024-06-24 10:58:18 +0900

    12.3 衝突判定を共通化（メソッド化）

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -1,13 +1,12 @@
 import java.util.Vector;
-
 public class GameFrame extends MyFrame {
 	public void run() {
-		GameWorld.player=new Player(100,300,0,0);
-		addKeyListener(GameWorld.player); // イベントリスナー登録(9章)
-		GameWorld.playerBullets=new Vector<PlayerBullet>();
-		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
-		while(true) {
+		GameWorld.player = new Player(100, 300, 0, 0);
+		GameWorld.playerBullets = new Vector<PlayerBullet>();
+		addKeyListener(GameWorld.player);
+		GameWorld.enemies = new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
+		while (true) {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
@@ -15,70 +14,75 @@ public class GameFrame extends MyFrame {
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+
 			sleep(0.03);
-	}
 		}
-	
+	}
 	public void movePlayerBullets() {
-		int i=0;
-			while (i<GameWorld.playerBullets.size()) {
-				PlayerBullet b=GameWorld.playerBullets.get(i);
-				b.draw(this);
-				b.move();
-				if (b.y<0) {
-					GameWorld.playerBullets.remove(i);
-				}else
-					i++;
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+			b.draw(this);
+			b.move();
+			if (b.y < 0) {
+				GameWorld.playerBullets.remove(i);
+			} else {
+				i++;
 			}
+		}
 	}
 	public void moveEnemies() {
-		for (int i=0 ; i<GameWorld.enemies.size(); i++) {
-			Enemy e=GameWorld.enemies.get(i);
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
 		}
-		
-		}
+	}
 	public void checkPlayerAndEnemies() {
-		for (int i=0 ; i<GameWorld.enemies.size(); i++) {
-			Enemy e=GameWorld.enemies.get(i);
-			if (Math.abs(e.x-GameWorld.player.x)<=30 &&
-				Math.abs(e.y-GameWorld.player.y)<=30) {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
+			if (Math.abs(e.x - GameWorld.player.x) <= 30 &&
+					Math.abs(e.y - GameWorld.player.y) <= 30) {
 				System.out.println("やられた!");
-				GameWorld.player.y=-1000;
+				GameWorld.player.y = -1000;
 			}
-	
 		}
-		}
+	}
+
 	public void checkPlayerBulletsAndEnemies() {
-		int i=0;
-		while (i<GameWorld.playerBullets.size()) {
-			//プレイヤー弾一つ一つについて、変数bに入れて繰り返し実行する
-			PlayerBullet b=GameWorld.playerBullets.get(i);
-			int j=0;
-			int hits=0;
-			while (j<GameWorld.enemies.size()) {
-				//敵一つ一つについて、変数eに入れて繰り返し実行する
-				Enemy e=GameWorld.enemies.get(j);
-				//敵eとプレイヤー弾bが衝突していたら「あたり」と表示
-				if(Math.abs(e.x-b.x)<=30 &&
-				   Math.abs(e.y-b.y)<=30) {
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+
+			// プレイヤー弾一つ一つについて、変数bに入れて繰り返し実行する
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+			int j = 0;
+			int hits = 0;
+			while (j < GameWorld.enemies.size()) {
+
+				// 敵一つ一つについて、変数eに入れて繰り返し実行する
+				Enemy e = GameWorld.enemies.get(j);
+
+				// 敵e とプレイヤー弾bが衝突していたら「あたり」を表示
+				if (Math.abs(e.x - b.x) <= 30 && Math.abs(e.y - b.y) <= 30) {
 					System.out.println("あたり");
 					hits++;
 					GameWorld.enemies.remove(j);
-				}else {
-				j++;
+				} else {
+					j++;
+				}
 			}
-			if(hits>0) {
+			if (hits > 0) {
 				GameWorld.playerBullets.remove(i);
 			} else {
-			    i++;
-		}
+				i++;
 			}
+		}
 	}
+		public boolean checkHit(Character a,Character b) { 
+			return Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30;
 }
-}
-	
+	}
+
 	
 
 
